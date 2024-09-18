dea0f17e6d206a77c68c8b9ae905824a4c2b1bb4
Jacob Prince <jacobpchandy2003@gmail.com>
2024-09-19 01:29:35 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:chatapp_flutter/auth/login_or_register.dart';
+import 'package:chatapp_flutter/firebase_options.dart';
 import 'package:chatapp_flutter/themes/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
